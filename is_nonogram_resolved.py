e4707fce957855eba0cde1ae22304f062d707cef
dphuong <dphuong>
2019-09-30 20:38:02 +0700

    done

--- is_nonogram_resolved.py
+++ is_nonogram_resolved.py
@@ -1,24 +1,4 @@
 #!/usr/bin/env python3
-
-def check_value(solution, len_rows, len_columns):
-    result = ()
-    for index in range(len_rows):
-        sum = 0
-        solution_row = ()
-        start = 0
-        while start < len_columns:
-            value = solution[index][start]
-            if value == 1:
-                sum += 1
-            elif value == 0 and sum != 0:
-                solution_row += (sum,)
-                sum = 0
-            start += 1
-        if sum != 0:
-            solution_row += (sum,)
-        result += (solution_row,)
-    print(result)
-
 def is_nonogram_resolved(specifications, solution):
     columns = specifications[0]
     rows = specifications[1]
@@ -26,8 +6,6 @@ def is_nonogram_resolved(specifications, solution):
     len_solution_rows = len(solution)
     solution_rows = ()
     solution_columns = ()
-
-    check_value(solution, len_solution_rows, len_solution_columns)
 
     # check solution rows
     for row_index in range(len_solution_rows):
@@ -45,9 +23,6 @@ def is_nonogram_resolved(specifications, solution):
         if sum != 0:
             solution_row += (sum,)
         solution_rows += (solution_row,)
-    # print(solution_rows)
-
-    
 
     # check solution columns
     for column_index in range(len_solution_columns):
@@ -65,7 +40,6 @@ def is_nonogram_resolved(specifications, solution):
         if sum != 0:
             solution_column += (sum,)
         solution_columns += (solution_column,)
-    # print(solution_columns)
 
     # check if solution equal to specifications
     if solution_columns == columns and solution_rows == rows:
@@ -95,7 +69,7 @@ def main():
         )
 
     print(is_nonogram_resolved(specifications, solution1))
-    # print(is_nonogram_resolved(specifications, solution2))
+    print(is_nonogram_resolved(specifications, solution2))
 
 if __name__ == '__main__':
     main()
